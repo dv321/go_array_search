0cc670a36b602286a80c97334ae6babf1a71cbc3
Doug Vargas <dvarg321@gmail.com>
2017-12-19 22:34:59 -0800

    doc fixes

--- solution.go
+++ solution.go
@@ -1,12 +1,12 @@
 package main
 
 /*
-Notes - This algorithm performs in O(N) time: there's one iteration of the array
-and hash table addition and lookup, which should usually be O(1) but in may be
-O(N) in the worst case scenario, according to the implementation. Hash table
-space complexity has a worst case of O(N). It seems as though the Go spec does
-not provide a guarantee on the run time complexity of the map type, so those are
-typical assumptions about performance.
+Notes - This algorithm performs in O(N) time: there's one O(N) iteration of the
+array and a hash table addition and lookup, which should usually be O(1) but may
+be O(N) in the worst case scenario, according to the implementation. Hash table
+space complexity has a worst case complexity of O(N). It seems as though the Go
+spec does not provide a guarantee on the run time complexity of the map type, so
+those are typical assumptions about performance.
 
 The algorithm works by setting every positive integer in the array as a key in a
 map, and then checking to see how many consecutive positive numbers, starting at
